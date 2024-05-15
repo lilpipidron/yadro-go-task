5ff4132f0f91fd29a17145187a0bc86b68b53ac6
lilpipidron <lociX55Lim@gmail.com>
2024-05-15 13:32:13 +0300

    feat: add param read

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"bufio"
+	"os"
+
+	"github.com/lilpipidron/yadro-go-task/internal/config"
+	"github.com/lilpipidron/yadro-go-task/internal/logger"
+)
+
 func main() {
+	log := logger.NewLogger()
+
+	if len(os.Args) != 2 {
+		log.Fatal("incorrect number of launch parameters")
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal("can't open file", err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	conf := config.MustLoad(reader, log)
+
+	log.Info(conf)
 }
