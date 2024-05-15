52a635d79586980036d70d6927f25e98ba47f7dc
lilpipidron <lociX55Lim@gmail.com>
2024-05-15 12:56:05 +0300

    feat: add TrimSpace to config params

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,11 +16,6 @@ type Config struct {
 }
 
 func MustLoad(reader *bufio.Reader) *Config {
-	amountTables, err := reader.ReadString('\n')
-	if err != nil {
-    log.Fatal("can't read table amount:", err)
-	}
-
 	conf := &Config{
 		Start: time.Time{},
 		End:   time.Time{},
@@ -28,32 +23,43 @@ func MustLoad(reader *bufio.Reader) *Config {
 		Cost:  0,
 	}
 
+	amountTables, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("can't read table amount: ", err)
+	}
+
+	amountTables = strings.TrimSpace(amountTables)
+
 	if conf.N, err = strconv.Atoi(amountTables); err != nil {
-    log.Fatal("incorrect table number format: ", err)
+		log.Fatal("incorrect table number format: ", err)
 	}
 
 	times, err := reader.ReadString('\n')
 	if err != nil {
-    log.Fatal("can't read time start and time and: ", err)
+		log.Fatal("can't read time start and time and: ", err)
 	}
+
+	times = strings.TrimSpace(times)
 
 	startEnd := strings.Split(times, " ")
 
 	if conf.Start, err = time.Parse("15:04", startEnd[0]); err != nil {
-    log.Fatal("incorrect start time format: ", err)
+		log.Fatal("incorrect start time format: ", err)
 	}
 
 	if conf.End, err = time.Parse("15:04", startEnd[1]); err != nil {
-    log.Fatal("incorrect end time format: ", err)
+		log.Fatal("incorrect end time format: ", err)
 	}
 
 	cost, err := reader.ReadString('\n')
 	if err != nil {
-    log.Fatal("can't read cost: ", err)
+		log.Fatal("can't read cost: ", err)
 	}
 
+	cost = strings.TrimSpace(cost)
+
 	if conf.Cost, err = strconv.Atoi(cost); err != nil {
-    log.Fatal("incorrect cost format: ")
+		log.Fatal("incorrect cost format: ")
 	}
 
 	log.Println("Configuration: ", conf)
