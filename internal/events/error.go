8d6cd4e62443d57ba62cea68b612413b3bf37a8c
lilpipidron <lociX55Lim@gmail.com>
2024-05-16 20:30:52 +0300

    feat: add custom errors

---
+++ internal/events/error.go
@@ -0,0 +1,9 @@
+package events
+
+const (
+	YouShallNotPass  = "YouShallNotPass"
+	NotOperYet       = "NotOperYet"
+	PlacelsBusy      = "PlacelsBusy"
+	ClientUnknown    = "ClientUnknown"
+	ICanWaitNoLonger = "ICanWaitNoLonger"
+)
