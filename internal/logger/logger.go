a05788e8be5d46354ffc088bfdcd9764672b40ac
lilpipidron <lociX55Lim@gmail.com>
2024-05-17 10:30:58 +0300

    feat: add error logger, prev error logger now fatal error logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -26,9 +26,21 @@ func (log *logger) Fatal(a ...interface{}) {
 		msg += fmt.Sprintf("%+v ", arg)
 	}
 
-	fmt.Printf("%s \033[31m[ERROR] %s\033[0m\n", timeStr, msg)
+	fmt.Printf("%s \033[31m[FATAL ERROR] %s\033[0m\n", timeStr, msg)
 
 	os.Exit(1)
+}
+
+func (log *logger) Error(a ...interface{}) {
+	t := time.Now()
+	timeStr := t.Format(time.RFC3339)
+
+	msg := ""
+	for _, arg := range a {
+		msg += fmt.Sprintf("%+v ", arg)
+	}
+
+	fmt.Printf("%s \033[33m[ERROR] %s\033[0m\n", timeStr, msg)
 }
 
 func (log *logger) Info(a ...interface{}) {
