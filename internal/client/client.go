680d6b19d5d915a634777711d7b652d2c6f2fed2
lilpipidron <lociX55Lim@gmail.com>
2024-05-17 12:22:05 +0300

    feat: add 12 event

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,6 +1,9 @@
 package client
 
+import "time"
+
 type Client struct {
+  Time  time.Time
 	Name  string
 	Table int
 }
--- internal/events/twelfth.go
+++ internal/events/twelfth.go
@@ -1,11 +1,23 @@
 package events
 
-import "time"
+import (
+	"time"
+
+	"github.com/lilpipidron/yadro-go-task/internal/club"
+	lg "github.com/lilpipidron/yadro-go-task/internal/logger"
+	"github.com/lilpipidron/yadro-go-task/internal/table"
+)
 
 type Twelfth struct {
-	Time  time.Time
-	Name  string
-	Table int
+	Time time.Time
+	Name string
 }
 
+func (event *Twelfth) Execution(club *club.Club, log lg.Log) {
+	tableID := <-club.EvalibleTables
+	log.Println(event.Time, 12, event.Name, tableID)
 
+	client := club.Clients[event.Name]
+	table := table.Table{Client: client, ID: tableID}
+	club.Tables[tableID] = table
+}
