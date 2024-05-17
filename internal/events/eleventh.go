edb9967c4e897952c9acd77c0bf6cd23991ebe80
lilpipidron <lociX55Lim@gmail.com>
2024-05-17 11:41:11 +0300

    feat: add 11 event

--- internal/events/eleventh.go
+++ internal/events/eleventh.go
@@ -3,6 +3,7 @@ package events
 import (
 	"time"
 
+	"github.com/lilpipidron/yadro-go-task/internal/club"
 	lg "github.com/lilpipidron/yadro-go-task/internal/logger"
 )
 
@@ -11,6 +12,11 @@ type Eleventh struct {
 	Name string
 }
 
-func (event *Eleventh) Execution(time time.Time, name string, log lg.Log) {
+func (event *Eleventh) Execution(time time.Time, name string, club *club.Club, log lg.Log) {
 	log.Println(time, 11, name)
+
+	client := club.Clients[name]
+	delete(club.Clients, name)
+
+	club.Earnings[client] = time
 }
